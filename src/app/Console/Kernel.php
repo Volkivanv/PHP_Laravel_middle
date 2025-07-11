6032d5506b5e68a261f3f3fcbae7ff022881789c
Ivan <volkivanv@mail.ru>
2025-07-12 00:09:21 +0300

    homework 10

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Jobs\ClearCache;
 use App\Jobs\SyncNews;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -15,12 +16,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(function(){
-            Log::info('Callback executed');
-        })->everyMinute();
-        $schedule->command('app:dump-database')->everyMinute();
-        $schedule->job(new SyncNews(15))->everyMinute();
-        $schedule->exec('touch storage/logs/test.log')->everyMinute();
+        // $schedule->call(function(){
+        //     Log::info('Callback executed');
+        // })->everyMinute();
+        // $schedule->command('app:dump-database')->everyMinute();
+        // $schedule->job(new SyncNews(15))->everyMinute();
+        // $schedule->exec('touch storage/logs/test.log')->everyMinute();
+        $schedule->job(ClearCache::class)->everyMinute();
     }
 
     /**
